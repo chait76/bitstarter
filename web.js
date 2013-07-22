d5f0cc1273b0b2b718d551df4d3c72455f07307e
Chaitanya Jee Srivastava <chait76@gmail.com>
2013-07-22 17:39:36 +0000

    Added modified index.html

--- web.js
+++ web.js
@@ -1,12 +1,5 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var fs = require('fs');
+var infile = "index.html";
+var buf = new Buffer(256);
+len = buf.toString("Hello World from index.html", "utf-8");
+console.log(fs.readFileSync(index.html, len));

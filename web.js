60b67da1b0f642fc93eda9f7a1ce57ba9d0512d1
Chaitanya Jee Srivastava <chait76@gmail.com>
2013-07-22 19:30:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,3 +1,14 @@
+var express = require('express');
 var fs = require('fs');
-var infile = "index.html";
-fs.readFileSync(infile);
+var htmlfile = "index.html"
+
+var app = express.createServer(express.logger());
+app.get('/', function(request, response){
+var html  = fs.readFileSync(htmlfile).toString();
+response.send(html);
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
+

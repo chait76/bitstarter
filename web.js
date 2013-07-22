0146661ee2fdfc2ee5347f5790143e356e9bc551
Chaitanya Jee Srivastava <chait76@gmail.com>
2013-07-22 19:08:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,4 +2,4 @@ var fs = require('fs');
 var infile = "index.html";
 var buf = new Buffer(256);
 len = buf.toString("Hello World from index.html", "utf-8");
-console.log(fs.readFileSync(index.html, len));
+console.log(fs.readFileSync(infile, len));

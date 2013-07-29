cebf41cb881dee22980d163e48f079f6663ed5d4
Ubuntu <ubuntu@ip-172-31-47-9.us-west-2.compute.internal>
2013-07-29 19:26:38 +0000

    added port 8080 output

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response){
 var html  = fs.readFileSync(htmlfile).toString();
 response.send(html);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log("Listening on " + port);
 });
